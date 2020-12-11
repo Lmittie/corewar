13a398a615e2ce1405c401bcdab32789be4f880a
acarlett <acarlett@student.21-school.ru>
2020-12-11 16:22:29 +0300

    11/12/20

--- srcs/handle_value.c
+++ srcs/handle_value.c
@@ -6,7 +6,7 @@
 /*   By: acarlett <acarlett@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/02 16:59:23 by lmittie           #+#    #+#             */
-/*   Updated: 2020/12/10 21:29:55 by acarlett         ###   ########.fr       */
+/*   Updated: 2020/12/11 16:06:08 by acarlett         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -68,6 +68,7 @@ void		place_value(int32_t arg, int32_t pos, t_carriage *carriage, t_data *data)
 	while (size--)
 	{
 		data->arena[get_pos(pos + size)] = arg & 0xFF;
+		data->code_color[get_pos(pos + size)] = carriage->color_code;
 		arg >>= (uint32_t)8;
 	}
 }
